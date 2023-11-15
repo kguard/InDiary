62de2ed5847200f4d4d54e8d247d74af49147ee2
김경호 <kkh9331@naver.com>
2023-11-16 05:26:22 +0900

    refactor version catalog 추가

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -53,11 +53,12 @@ android {
 
 dependencies {
 
-    implementation ("androidx.core:core-ktx:1.12.0")
+    //implementation ("androidx.core:core-ktx:1.12.0")
+    implementation(libs.androidx.core.ktx)
     implementation ("androidx.appcompat:appcompat:1.6.1")
     implementation ("com.google.android.material:material:1.10.0")
     implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("androidx.legacy:legacy-support-v4:1.0.0")
+    //implementation ("androidx.legacy:legacy-support-v4:1.0.0")
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
@@ -110,7 +111,7 @@ dependencies {
 
     //Dagger Hilt
     implementation ("com.google.dagger:hilt-android:2.46.1")
-    kapt ("com.google.dagger:hilt-android-compiler:2.46.1")
+    kapt ("com.google.dagger:hilt-compiler:2.46.1")
     kapt ("com.google.dagger:hilt-android-compiler:2.46.1")
     //implementation "androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03"
     //kapt ("androidx.hilt:hilt-compiler:1.1.0")
