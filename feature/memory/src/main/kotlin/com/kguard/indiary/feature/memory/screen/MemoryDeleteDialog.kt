beda031376a65042a5b47d3fa9ab057abdf3c919
김경호 <kkh9331@naver.com>
2024-01-04 23:00:56 +0900

    [compose] personDeleteDialog 마무리 및 memoryDeleteDialog 제작

--- feature/memory/src/main/kotlin/com/kguard/indiary/feature/memory/screen/MemoryDeleteDialog.kt
+++ feature/memory/src/main/kotlin/com/kguard/indiary/feature/memory/screen/MemoryDeleteDialog.kt
@@ -1,6 +1,53 @@
 package com.kguard.indiary.feature.memory.screen
 
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.kguard.indiary.core.designsystem.R
+import com.kguard.indiary.core.designsystem.theme.IndiaryTheme
 
 @Composable
-fun MemoryDeleteDialog(){}
+fun MemoryDeleteDialog(
+    memoryName: String,
+    onConfirmation: () -> Unit,
+    onDismissRequest: () -> Unit
+) {
+    AlertDialog(
+        title = {
+            Text(
+                text = memoryName,
+                style = MaterialTheme.typography.displayLarge
+            )
+        },
+        text = {
+            Text(
+                text = stringResource(id = R.string.DeleteMemory),
+                color = MaterialTheme.colorScheme.onPrimaryContainer
+            )
+        },
+        onDismissRequest = { onDismissRequest() },
+        confirmButton = {
+            TextButton(onClick = { onConfirmation() }) {
+                Text(text = stringResource(id = R.string.Delete))
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = { onDismissRequest() }) {
+                Text(text = stringResource(id = R.string.Cancel))
+            }
+        }
+    )
+
+}
+
+@Preview
+@Composable
+fun MemoryDeleteDialogPrv() {
+    IndiaryTheme {
+        MemoryDeleteDialog(memoryName = "집에 가기", onConfirmation = {}, onDismissRequest = {})
+    }
+}
--- feature/person/src/main/kotlin/com/kguard/indiary/feature/person/screen/PersonDeleteDialog.kt
+++ feature/person/src/main/kotlin/com/kguard/indiary/feature/person/screen/PersonDeleteDialog.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -15,45 +17,50 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.kguard.indiary.core.designsystem.theme.IndiaryTheme
 import com.kguard.indiary.core.designsystem.R
 
 @Composable
-fun PersonDeleteDialog(){
-
-}
-
-@Composable
-fun PersonDeleteDialogContents(
-    onClick : () -> Unit,
-    onDismiss: () -> Unit
-){
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(modifier = Modifier.padding(32.dp),text = stringResource(id = R.string.DeletePerson), style = MaterialTheme.typography.labelLarge)
-        Row(
-            modifier = Modifier.fillMaxWidth().padding(8.dp),
-            horizontalArrangement = Arrangement.End
-        ) {
-            TextButton(onClick = onClick) {
-                Text(text = stringResource(id = R.string.Cancel))
-            }
-            TextButton(onClick = onDismiss) {
+fun PersonDeleteDialog(
+    personName: String,
+    onConfirmation: () -> Unit,
+    onDismissRequest: () -> Unit
+) {
+    AlertDialog(
+        title = {
+            Text(
+                text = personName,
+                style = MaterialTheme.typography.displayLarge
+            )
+        },
+        text = {
+            Text(
+                text = stringResource(id = R.string.DeletePerson),
+                color = MaterialTheme.colorScheme.onPrimaryContainer
+            )
+        },
+        onDismissRequest = { onDismissRequest() },
+        confirmButton = {
+            TextButton(onClick = { onConfirmation() }) {
                 Text(text = stringResource(id = R.string.Delete))
             }
+        },
+        dismissButton = {
+            TextButton(onClick = { onDismissRequest() }) {
+                Text(text = stringResource(id = R.string.Cancel))
+            }
         }
-    }
+    )
+
 }
 
 @Preview
 @Composable
-fun PersonDeletePrev(){
+fun PersonDeletePrev() {
     IndiaryTheme {
-        PersonDeleteDialogContents()
+        Column{
+           PersonDeleteDialog(personName = "김경호",onConfirmation = {}, onDismissRequest = {})
+        }
     }
 }
